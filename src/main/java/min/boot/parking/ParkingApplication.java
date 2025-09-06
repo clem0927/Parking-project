8d2515a644359e04ece42345b4dd54bd7151fe94
clem0927 <minyoung09277@naver.com>
2025-09-06 13:46:17 +0900

    Initial commit

---
+++ src/main/java/min/boot/parking/ParkingApplication.java
@@ -0,0 +1,13 @@
+package min.boot.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingApplication {
+    //dd
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingApplication.class, args);
+    }
+
+}
