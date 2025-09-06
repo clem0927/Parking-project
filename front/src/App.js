f8dc580ea82353579e468b3cd75a05e729157bd3
clem0927 <minyoung09277@naver.com>
2025-09-06 13:59:21 +0900

    add front

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import MapContainer from "./MapContainer";
+
+function App() {
+  return (
+    <div>
+      <h1>Paking</h1>
+      <MapContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import KakaoMap from "./KaKaoMap";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<KakaoMap />);
